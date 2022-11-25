4df776b4db23db0ef5333accbac1bfb61702ee86
bri <bcamesi@gmail.com>
2022-11-25 15:29:07 -0500

    /animals index works, seeded data

--- models/seed.js
+++ models/seed.js
@@ -8,6 +8,26 @@ mongoose.connection.on("open", () => {
         extinct: false, 
         location: "Worldwide",
         lifeExpectancy: 79 },
+
+        { species: "Parastratiosphecomyia stratiosphecomyioides", 
+        extinct: false, 
+        location: "Thailand",
+        lifeExpectancy: 0.12 },
+
+        { species: "Pica pica", 
+        extinct: false, 
+        location: "Eurasia",
+        lifeExpectancy: 4 },
+
+        { species: "Aha ha", 
+        extinct: false, 
+        location: "Australia",
+        lifeExpectancy: 0.03 },
+
+        { species: "Vini vidivici", 
+        extinct: true, 
+        location: "Polynesia",
+        lifeExpectancy: 10 },
       ]
 
       Animal.deleteMany({}, (err, data) => {
